2e60a932c609e19d5a7ce09b474cf2607e459ead
James Pierce <mr.jpierce@gmail.com>
2017-01-09 10:34:57 +0900

    Added mocha-itcase and changed tests to utilize

--- test/crono.tests.js
+++ test/crono.tests.js
@@ -4,6 +4,8 @@ const sinon = require('sinon');
 const pronto = require('./../lib/pronto');
 const Crono = pronto.Crono;
 
+const itCase = require('mocha-itcase');
+
 describe('Crono', () => {
 
     let sutFunctionSpy = sinon.spy();
@@ -29,41 +31,33 @@ describe('Crono', () => {
         assert(sutFunctionSpy.calledOnce);
     });
 
-    [
+    itCase('passes arguments to fut', [
         {args: [1,2,3], expected: [1,2,3]},
         {args: [4,5,6], expected: [4,5,6]}
-    ]
-    .forEach((test) => {
-        let { args, expected } = test;
-        it('passes arguments to fut', () => {
-            // Arrange
-            let crono = new Crono(testObject, 'fut', 1);
+    ], (args, expected) => {
+         // Arrange
+        let crono = new Crono(testObject, 'fut', 1);
 
-            // Act
-            testObject.fut(...args);
+        // Act
+        testObject.fut(...args);
 
-            // Assert
-            assert(sutFunctionSpy.calledWithExactly(...expected));
-        });
+        // Assert
+        assert(sutFunctionSpy.calledWithExactly(...expected));
     });
 
-    [
+    itCase('calls fut the amount of times passed to constructor', [
         {repeatCount: 1, expected: 1},
         {repeatCount: 2, expected: 2},
         {repeatCount: null, expected: 10} // default
-    ]
-    .forEach((test) => {
-        let { repeatCount, expected } = test;
-        it(`calls fut the amount of times passed to constructor (${repeatCount}, ${expected})`, () => {
-            // Arrange
-            let crono = new Crono(testObject, 'fut', repeatCount);
+    ], (repeatCount, expected) => {
+        // Arrange
+        let crono = new Crono(testObject, 'fut', repeatCount);
 
-            // Act
-            testObject.fut(1, 2, 3);
+        // Act
+        testObject.fut(1, 2, 3);
 
-            // Assert
-            assert.equal(sutFunctionSpy.callCount, expected);
-        });
+        // Assert
+        assert.equal(sutFunctionSpy.callCount, expected);
     });
 
 /*    it('calls setup fuandfajdjfnon', () => {
